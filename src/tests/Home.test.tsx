6606f542d2f5820271d73842ef7c70919bf50d6e
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-28 12:03:26 -0300

    chore: vitest with testing library

---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import Home from '../pages';
+
+test('home', () => {
+  render(<Home />);
+  expect(screen.getByText('Next Vitest Boilerplate')).toBeInTheDocument();
+});
